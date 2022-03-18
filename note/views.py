4d695ce80abce4a3aad1f360027d84fef184cdac
Reisuke1231 <reisuke1231@gmail.com>
2022-03-18 13:36:48 +0900

    Partialize root settings for each application

--- note/views.py
+++ note/views.py
@@ -1,3 +1,19 @@
+from django.http import Http404
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Note
+
+
+def list(request):
+    notes = Note.objects.all()
+
+    return render(request, 'note/list.html', {'notes': notes})
+
+
+def detail(request, note_id):
+    try:
+        note = Note.objects.get(id=note_id)
+    except Note.DoesNotExist:
+        raise Http404("Note doesn't exist")
+
+    return render(request, 'note/detail.html', {'note': note})
--- smartnotes/urls.py
+++ smartnotes/urls.py
@@ -14,12 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from home import views as home_views
+from note import views as note_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_views.home),
-    path('authorized', home_views.authorized)
+    path('', include('home.urls')),
+    path('note/', include('note.urls')),
 ]
