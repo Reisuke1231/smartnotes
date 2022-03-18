3a1c223febbec268210eab4d24990be6cd5a86ca
Reisuke1231 <reisuke1231@gmail.com>
2022-03-18 15:47:17 +0900

    Styling with Bootstrap

--- note/urls.py
+++ note/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('list', views.NoteListView.as_view(), name='list'),
-    path('<int:pk>', views.NoteDetailView.as_view(), name='detail'),
+    path('list', views.NoteListView.as_view(), name='note.list'),
+    path('<int:pk>', views.NoteDetailView.as_view(), name='note.detail'),
 ]
