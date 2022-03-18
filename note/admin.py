7905d10933ecfbf4f7f4afce9dd8de01719d5a72
Reisuke1231 <reisuke1231@gmail.com>
2022-03-18 12:40:42 +0900

    Add Note model and set config for model

--- note/admin.py
+++ note/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+
+admin.site.register(models.Note, NoteAdmin)
