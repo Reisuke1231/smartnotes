869c8b7fa983d4e931789bcf61af5af9f7033d9b
Reisuke1231 <reisuke1231@gmail.com>
2022-03-18 12:18:48 +0900

    Implement home and authorized

--- smartnotes/urls.py
+++ smartnotes/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from home import views as home_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_views.home),
+    path('authorized', home_views.authorized)
 ]
