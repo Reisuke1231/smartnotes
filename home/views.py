ec89a85ad5d2a23fc8dbe16646d3c024d5cea3c4
Reisuke1231 <reisuke1231@gmail.com>
2022-03-19 13:57:20 +0900

    Implement signup screen

--- home/views.py
+++ home/views.py
@@ -1,9 +1,25 @@
+from urllib import request
 from django.shortcuts import render
 from datetime import datetime
 from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
+from django.views.generic.edit import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
+
+from django.shortcuts import redirect
+
+
+class SignupView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'home/register.html'
+    success_url = '/login'
+
+    def get(self, redirect, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('note/list')
+        return super().get(request, *args, **kwargs)
 
 
 class LoginInterfaceView(LoginView):
