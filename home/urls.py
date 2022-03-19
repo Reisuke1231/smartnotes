08d9286eff2d729bda6c42f652f50657c2a819cf
Reisuke1231 <reisuke1231@gmail.com>
2022-03-19 13:39:19 +0900

    Set LoginRequiredMixin to logout & Delete authorized action

--- home/urls.py
+++ home/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
-    path('authorized', views.AuthorizedView.as_view(), name='authorized'),
     path('login', views.LoginInterfaceView.as_view(), name='login'),
     path('logout', views.LogoutInterfaceView.as_view(), name='logout'),
 ]
--- home/views.py
+++ home/views.py
@@ -6,20 +6,16 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
 
 
-class LogoutInterfaceView(LogoutView):
-    template_name = 'home/logout.html'
-
-
 class LoginInterfaceView(LoginView):
     template_name = 'home/login.html'
+
+
+class LogoutInterfaceView(LoginRequiredMixin, LogoutView):
+    template_name = 'home/logout.html'
+    login_url = '/login'
 
 
 class HomeView(LoginRequiredMixin, TemplateView):
     template_name = 'home/welcome.html'
     extra_context = {'today': datetime.today()}
     login_url = '/login'
-
-
-class AuthorizedView(LoginRequiredMixin, TemplateView):
-    template_name = 'home/authorized.html'
-    login_url = '/admin'
