2f56a12199d405e6d98dbf9d885e8b74df24ca3c
Reisuke1231 <reisuke1231@gmail.com>
2022-03-19 13:09:00 +0900

    Set up associations between note model and user model

--- note/views.py
+++ note/views.py
@@ -1,8 +1,9 @@
 from typing import List
-from django.http import Http404
+from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import render
 from django.views.generic import CreateView, DetailView, ListView, UpdateView
 from django.views.generic.edit import DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .forms import NoteForm
 from .models import Note
@@ -27,11 +28,22 @@ class NoteCreateView(CreateView):
     form_class = NoteForm
     success_url = '/note/list'
 
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
 
-class NoteListView(ListView):
+        return HttpResponseRedirect(self.get_success_url())
+
+
+class NoteListView(LoginRequiredMixin, ListView):
     model = Note
     template_name = 'note/list.html'
     context_object_name = 'notes'
+    login_url = '/admin'
+
+    def get_queryset(self):
+        return self.request.user.note.all()
 
 
 class NoteDetailView(DetailView):
