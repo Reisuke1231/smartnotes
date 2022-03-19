4e2be9bf981d79a5029a5911964a7d759c5986b4
Reisuke1231 <reisuke1231@gmail.com>
2022-03-19 12:09:53 +0900

    Add path for update

--- note/urls.py
+++ note/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('list', views.NoteListView.as_view(), name='note.list'),
     path('<int:pk>', views.NoteDetailView.as_view(), name='note.detail'),
+    path('<int:pk>/edit', views.NoteUpdateView.as_view(), name='note.update'),
     path('new', views.NoteCreateView.as_view(), name='note.new'),
 ]
