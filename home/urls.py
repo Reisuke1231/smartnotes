246fffee9ba13e75d33ea9e21a09c171c0d9f1bc
Reisuke1231 <reisuke1231@gmail.com>
2022-03-19 13:31:09 +0900

    Implement user logout screen

--- home/urls.py
+++ home/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home', views.HomeView.as_view(), name='home'),
+    path('', views.HomeView.as_view(), name='home'),
     path('authorized', views.AuthorizedView.as_view(), name='authorized'),
-    path('login', views.LoginInterfaceView.as_view()),
+    path('login', views.LoginInterfaceView.as_view(), name='login'),
+    path('logout', views.LogoutInterfaceView.as_view(), name='logout'),
 ]
