389ec6a2869f677018f9cacc06b5ade92bb657ca
Reisuke1231 <reisuke1231@gmail.com>
2022-03-19 12:21:21 +0900

    Add path for delete

--- note/urls.py
+++ note/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('list', views.NoteListView.as_view(), name='note.list'),
     path('<int:pk>', views.NoteDetailView.as_view(), name='note.detail'),
     path('<int:pk>/edit', views.NoteUpdateView.as_view(), name='note.update'),
+    path('<int:pk>/delete', views.NoteDeleteView.as_view(), name='note.delete'),
     path('new', views.NoteCreateView.as_view(), name='note.new'),
 ]
